fad71bf5d54c0a5b61a8899c5fb303cbb7c5d8eb
Dariel Urbina <darielu@gearfire.com>
2021-01-06 09:01:26 -0500

    adding structure of map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from "../environments/environment";
+import * as Mapboxgl from 'mapbox-gl';
+import { stores } from './services/data'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {    
+  map: Mapboxgl.Map;  
+  myStores = stores;
+
+  ngOnInit(): void {
+    Mapboxgl.accessToken = environment.accessToken;
+
+    this.map = new Mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-77.034084, 38.909671],
+      zoom: 14
+    });   
+
+    this.map.on('load', () => {
+      /* Add the data to your map as a layer */      
+
+      this.map.addLayer({
+        "id": "locations",
+        "type": "symbol",
+        /* Add a GeoJSON source containing place coordinates and information. */
+        "source": {
+          "type": "geojson",
+          "data": this.myStores
+        },
+        "layout": {
+          "icon-image": "restaurant-15",
+          "icon-allow-overlap": true,
+        }
+      });
+    });
+  }  
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken: 'pk.eyJ1IjoiYmV0YXVwc3g4NiIsImEiOiJja2plbnh5aHowcGpxMnpwZGEyczZ1bnQ5In0.H4YZytBX6Kj2BmijlGBkYw'
 };
 
 /*
